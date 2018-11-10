2e15ceebbbdc46108646f1a1b0c41d65d442e34e
puretree <44718505+puretree@users.noreply.github.com>
2018-11-10 15:12:23 +0800

    Add files via upload

---
+++ mongodb.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 25 00:11:54 2018
+
+@author: linzino
+"""
+
+from pymongo import MongoClient
+import pymongo
+import urllib.parse
+from datetime import datetime 
+
+
+
+# db setting
+host = 'host'
+port = 'PORT'
+username = urllib.parse.quote_plus('帳號')
+password = urllib.parse.quote_plus('密碼')
+# Authentication Database
+Authdb='資料庫'
+
+
+def init_db():
+    client = MongoClient('mongodb://%s:%s@%s:%s/%s?authMechanism=SCRAM-SHA-1'
+                      % (username, password, host, port, Authdb))
+
+    db = client[Authdb]
+    return db
+
+
+def insert_one(dic,collection):
+    #collection_name = 'users'
+    db = init_db()
+    coll = db[collection]
+    coll.insert_one(dic)
+
+
+def get_all(collection):
+    db = init_db()
+    coll = db[collection]
+    return list(coll.find())
+
+def find_user(userid,collection):
+    '''
+    確認這個使用者是不是加入了
+    '''
+    db = init_db()
+    coll = db[collection]
+    return len(list(coll.find({"userid":userid})))
+
+
+
